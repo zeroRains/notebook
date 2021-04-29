dca035d79870b8765434eb543a2ea6d709acceed
Therainisme <me@Therainisme.com>
2021-04-29 10:37:43 +0800

    添加 vuepress-markdown-enhance以支持Tex语法

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,3 @@
-
 const { getSidebar } = require('../../js/index');
 
 module.exports = {
@@ -20,14 +19,29 @@ module.exports = {
             {
                 text: '理论课程',
                 items: [
-                    { text: '数据库', link: '/database/<1>绪论' },
+                    { text: '数据库系统概论', link: '/database/<1>绪论' },
+                ]
+            },
+            {
+                text: '游泳',
+                items: [
+                    { text: '算法', link: '/algorithm/<1>搜索与图论' },
                 ]
             }
         ],
         // 为以下路由添加侧边栏
         sidebar: {
-            '/database/': getSidebar('database', '数据库')
+            '/database/': getSidebar('database'),
+            '/algorithm/': getSidebar('algorithm')
         }
-    }
+    },
+    plugins: [
+        [
+            "md-enhance",
+            {
+                // 启用 TeX 支持
+                tex: true,
+            },
+        ],
+    ],
 }
-
