51c7ec9f4f836b2d1c8a29e62373d03f9e2daf09
Therainisme <me@Therainisme.com>
2021-05-01 22:58:30 +0800

    质数 质因数 因数 最大公约数 最小公倍数

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,12 +5,13 @@ module.exports = {
         lineNumbers: false
     },
     title: '唤雨考拉的笔记本',
-    description: '没用的笔记本，相信没人会看的',
+    description: '对于唤雨考拉来说，这是一个百宝箱，但对于其他人来说，可能是一本用处不大的笔记本。',
     head: [
         ['link', {
             rel: 'icon',
             href: '/logo.png'
-        }]
+        }],
+        ['meta', { name: 'keywords', content: 'notebook,Therainisme,Algorithm,题解,算法,编程,学习笔记' }]
     ],
     themeConfig: {
         // 添加导航栏
